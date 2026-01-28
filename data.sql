f4701bbe0675e1141fa4d5c592cdccc97d81e28d
Srivamshi Voggu <srivamshivoggu@gmail.com>
2026-01-28 23:09:45 +0530

    Add initial data for customers, products, and orders

---
+++ data.sql
@@ -0,0 +1,20 @@
+INSERT INTO customers VALUES
+(1,'Ravi','Hyderabad'),
+(2,'Anil','Delhi'),
+(3,'Sita','Mumbai'),
+(4,'Kiran','Hyderabad'),
+(5,'Neha','Chennai');
+
+INSERT INTO products VALUES
+(101,'Laptop','Electronics',50000),
+(102,'Phone','Electronics',20000),
+(103,'Headphones','Accessories',2000),
+(104,'Keyboard','Accessories',1500);
+
+INSERT INTO orders VALUES
+(1,1,101,1,'2025-01-01'),
+(2,1,103,2,'2025-01-05'),
+(3,2,102,1,'2025-01-03'),
+(4,3,103,3,'2025-01-10'),
+(5,4,101,1,'2025-01-15'),
+(6,2,104,2,'2025-01-20');
